b7c1bd54b9ee7a35cc7d8cb7e1815740c6195e1a
emanovic7 <e_owusu@live.com>
2019-11-20 21:22:46 -0500

    install material ui, update About page, update navbar

--- src/components/about.js
+++ src/components/about.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import MuiLink from '@material-ui/core/Link';
+
+const copyRight = () => {
+  return(
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <MuiLink color="inherit" href="https://material-ui.com/">
+        Your Website
+      </MuiLink>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
 
 class About extends Component {
   render(){
     return (
       <div className="about">
-        I am the about page!
-        <div className="homeImage">
-          <img style={{width: '200px', height: '200 px'}} src="/images/homePageImage.jpg"/>
-        </div>
+        <Container>
+          <div className="homeImage">
+            <img style={{width: '200px', height: '200 px'}} src="/images/homePageImage.jpg"/>
+          </div>
+        </Container>
+          <copyRight />
       </div>
     )
   }
--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 
 
 class NavBar extends Component {
+
   render(){
     return(
       <div className="navBar">
-        I am the NavBar!
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Navbar.Brand href="#home">Home</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">About</Nav.Link>
+            <Nav.Link href="#experience">Experience</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar>
       </div>
     )
   }
