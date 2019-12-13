9f787e4a61da232f327dcb1f107bb92fbd18d953
emanovic7 <e_owusu@live.com>
2019-12-13 17:26:22 -0500

    update about.js to boostrap media

--- src/components/about.js
+++ src/components/about.js
@@ -1,93 +1,40 @@
 import React, { Component } from 'react';
-import Container from '@material-ui/core/Container';
-import MuiLink from '@material-ui/core/Link';
-import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import ButtonBase from '@material-ui/core/ButtonBase';
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    margin: 'auto',
-    maxWidth: 500,
-  },
-  image: {
-    width: 128,
-    height: 128,
-  },
-  img: {
-    margin: 'auto',
-    display: 'block',
-    maxWidth: '100%',
-    maxHeight: '100%',
-  },
-}));
-
-const copyRight = () => {
-  return(
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <MuiLink color="inherit" href="https://material-ui.com/">
-        Your Website
-      </MuiLink>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+import Media from 'react-bootstrap/Media';
 
 
-export default function About() {
 
-   const classes = useStyles();
 
+
+class About extends Component {
+
+  render(){
 
     return (
       <div className="about">
-      <Paper className={classes.paper}>
-        <Grid container spacing={2}>
 
-        <Grid item>
-            <div className="bio">
-              <h3>About Me</h3>
-              <p>
-                Hello, I am Emmanuel! I am a Software Engineer from Ghana based in NYC. I enjoy building all things complex
-                and simple, especially with code. My background is in Physics, and I have worked in Engineering, my
-                favourite being Solar Engineering. Some of my passions are Green Energy.
-
-                These are some techonologies I have been working with
-                  <div className="tech-stack-list">
-                    <ul>
-                      <li>JavaScript ES6+</li>
-                      <li>React + Redux</li>
-                      <li>Node.js</li>
-                      <li>Ruby, Ruyb on Rails</li>
-                      <li>HTML 5, (S)CSS</li>
-                      <li>SQL, PostgreSQL</li>
-                    </ul>
-                And some other engineering technologies I enjoy
-                     <ul>
-                      <li>Matlab</li>
-                      <li>CAD, 3D && 2D</li>
-                     </ul>
-                  </div>
-              </p>
-            </div>
-          </Grid>
-          <Grid item>
-            <div className="homeImage">
-              <img style={{width: '200px', height: '200 px'}} src="/images/homePageImage.jpg"/>
-            </div>
-          </Grid>
-
-          </Grid>
-        </Paper>
-          <copyRight />
+      <Media>
+        <img
+          width={300}
+          height={300}
+          className="mr-3"
+          src="/images/homePageImage.jpg"
+          alt="Generic placeholder"
+        />
+        <Media.Body>
+        <h3>About Me</h3>
+          <p>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+            ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
+            tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.
+            Donec lacinia congue felis in faucibus.
+          </p>
+        </Media.Body>
+      </Media>
       </div>
     )
+
+  }
+
 }
+
+export default About;
